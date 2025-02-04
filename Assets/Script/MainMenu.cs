7d05534b6e3d4e9b700b65a3bc70ce84e91784c8
Sam <75374659+Sam123www@users.noreply.github.com>
2025-02-04 23:21:21 +0800

    2025/02/04

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneAsset playScene, settingScene, aboutAsScene;
+    public void playBotton()
+    {
+        SceneManager.LoadScene(playScene.name);
+    }
+    public void settingBotton()
+    {
+        SceneManager.LoadScene(settingScene.name);
+    }
+    public void aboutAsBotton()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+    public void quitBotton()
+    {
+        Application.Quit();
+    }
+}
+ 
