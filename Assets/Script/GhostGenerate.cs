f320dbb80a52b5f007df4228378feb72fbc67d7d
Sam <75374659+Sam123www@users.noreply.github.com>
2025-03-08 01:44:00 +0800

    2025/3/8

--- Assets/Script/GhostGenerate.cs
+++ Assets/Script/GhostGenerate.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -10,6 +11,7 @@ public class GhostGenerate : MonoBehaviour
     public GameObject ghost;
     public float cycleTime;
     float Timer;
+    bool inRange;
     public int num_max;
     int num = 0;
 
@@ -19,11 +21,19 @@ public class GhostGenerate : MonoBehaviour
     }
     void Update()
     {
-        if(Time.time >= Timer && num <= num_max)
+        if(Time.time >= Timer && num <= num_max && inRange)
         {
             Instantiate(ghost, transform.position, Quaternion.Euler(0, 0, 0));
             Timer += cycleTime;
             num++;
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inRange = false;
     }
 }
